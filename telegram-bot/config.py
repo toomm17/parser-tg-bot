df4a0516d22c986995f1bc02a99e8d86d43bf78b
Too Much Fun <mihail.karpinskii@mail.ru>
2024-03-02 13:07:15 +0300

    Added config with creds from .env file

---
+++ telegram-bot/config.py
@@ -0,0 +1,11 @@
+import os 
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', '')
+
+BASE_DIR = Path(__file__).resolve().parent
